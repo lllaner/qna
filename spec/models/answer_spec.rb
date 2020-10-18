839de85d0a50ea103f777a1a8098e6a583d2b62e
Laner <iifunnymomen@gmail.com>
2020-10-18 15:26:17 +0300

    Answer model with spec

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  describe 'associations' do
+    it { should belong_to(:question).class_name('Question') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :body }
+  end
+
+end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
+  describe 'associations' do
+    it { should have_many(:answers).class_name('Answer') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+  end
 end
